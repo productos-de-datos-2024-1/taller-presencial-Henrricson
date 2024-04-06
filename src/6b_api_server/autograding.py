c71cceaae6043e20a9b136ed5f79ac3c7ae9d777
Henrry Cabrera <hcabrerao@unal.edu.co>
2024-04-06 13:03:07 -0500

    Intento3

--- src/6b_api_server/autograding.py
+++ src/6b_api_server/autograding.py
@@ -5,7 +5,5 @@ import os
 # test code files
 assert os.path.exists("src/6b_api_client/config.json")
 assert os.path.exists("src/6b_api_client/server.py")
-
-
-# test run
+# test run 1
 assert os.path.exists("datalake/logs/api_client.log")
